7f8d1637cef3508da1b5d779c7318f80ce0f0647
Alexei Petrov <petrovAlekse95@mail.ru>
2025-01-25 19:42:31 +0300

    Initial commit

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+
+class Figure {
+protected:
+    std::string _name;
+    int _numberSides;
+    int getNumberSides();
+    std::string getName();
+public:
+    Figure() : _name("Фигура"), _numberSides(0){};
+    void printData();
+};
+
+class Triangle : public Figure {
+public:
+    Triangle() : Figure() {
+        _name = "Треугольник";
+        _numberSides = 3;
+    };
+};
+
+class Quadrilateral : public Figure {
+public:
+    Quadrilateral() : Figure() {
+        _name = "Четырёхугольник";
+        _numberSides = 4;
+    };
+};
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    Figure figure;
+    Triangle triangle;
+    Quadrilateral quadrilateral;
+
+    std::cout << "Количество сторон:" << std::endl;
+    figure.printData();
+    triangle.printData();
+    quadrilateral.printData();
+};
+
+
+
+int Figure::getNumberSides() {
+    return _numberSides;
+};
+
+std::string Figure::getName() {
+    return _name;
+};
+
+void Figure::printData() {
+    std::cout << _name << ": " << _numberSides << std::endl;
+}
